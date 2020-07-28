ee6b7ae426cc774e6dd356a0906ff0cac760272c
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-07-27 23:43:18 -0700

    added the skeleton for prediction.cpp

--- prediction.cpp
+++ prediction.cpp
@@ -1,11 +1,14 @@
-#include <stdio.h>
+#include <cstdio>
+#include <string>
 #include "pyhelper.hpp"
 
 int main(int argc, char* argv[]) {
 	CPyInstance pyInstance;
 
-	PyRun_SimpleString("print('Hello World from Embedded Python!!!')");
+	// load prediction.py
+	const char filename[] = "prediction.py";
+	FILE *fp = _Py_fopen(filename, "r");
+	PyRun_SimpleFile(fp, filename);
 
-	printf("\nPress any key to exit...\n");
 	return 0;
 }
